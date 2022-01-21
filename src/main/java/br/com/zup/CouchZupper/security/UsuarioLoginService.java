a5669a043d344b699ee2d1b4a4d55cd878fbbe97
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-21 14:49:46 -0300

    Configurar UserDetailsService para buscar usuário via email

--- src/main/java/br/com/zup/CouchZupper/security/UsuarioLoginService.java
+++ src/main/java/br/com/zup/CouchZupper/security/UsuarioLoginService.java
@@ -1,4 +1,26 @@
 package br.com.zup.CouchZupper.security;
 
-public class UsuarioLoginService {
+import br.com.zup.CouchZupper.usuario.Usuario;
+import br.com.zup.CouchZupper.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UsuarioLoginService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(email);
+
+        optionalUsuario.orElseThrow(() -> new UsernameNotFoundException("Dados de login inseridos incorretamente"));
+        Usuario usuario = optionalUsuario.get();
+
+        return new UsuarioLogado(usuario.getId(), usuario.getEmail(), usuario.getSenha());
+
+    }
 }
