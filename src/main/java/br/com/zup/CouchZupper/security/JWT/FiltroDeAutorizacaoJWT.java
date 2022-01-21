1c5bd7e713e07af9e57ff3ba8c2721a78d25f6ec
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-21 15:34:18 -0300

    Criar filtro de autorização JWT

--- src/main/java/br/com/zup/CouchZupper/security/JWT/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/CouchZupper/security/JWT/FiltroDeAutorizacaoJWT.java
@@ -1,4 +1,18 @@
 package br.com.zup.CouchZupper.security.JWT;
 
-public class FiltroDeAutorizacaoJWT {
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
+
+    private JWTComponent jwtComponent;
+    private UserDetailsService userDetailsService;
+
+    public FiltroDeAutorizacaoJWT(AuthenticationManager authenticationManager, JWTComponent jwtComponent, UserDetailsService userDetailsService) {
+        super(authenticationManager);
+        this.jwtComponent = jwtComponent;
+        this.userDetailsService = userDetailsService;
+    }
 }
