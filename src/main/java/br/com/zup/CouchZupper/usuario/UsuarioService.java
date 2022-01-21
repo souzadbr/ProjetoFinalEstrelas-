6a698f6b9199e39b0100c8274fcaa2e0f90b74fa
vanessalb08 <vanessalimab08@gmail.com>
2022-01-21 16:00:40 -0300

    Cria metodo de salvar usuario

--- src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
@@ -1,4 +1,16 @@
 package br.com.zup.CouchZupper.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvarUsuario(Usuario novoUsuario){
+        return usuarioRepository.save(novoUsuario);
+
+    }
 }
