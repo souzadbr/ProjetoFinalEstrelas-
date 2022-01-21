b9e6128cced72c8fc1f117580a080be48f84adda
Debora Souza <deboral.p@hotmail.com>
2022-01-21 14:31:19 -0300

    Criar método gerarToken

--- src/main/java/br/com/zup/CouchZupper/security/JWT/JWTComponent.java
+++ src/main/java/br/com/zup/CouchZupper/security/JWT/JWTComponent.java
@@ -1,4 +1,25 @@
 package br.com.zup.CouchZupper.security.JWT;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JWTComponent {
+
+    @Value("${jwt.segredo}")
+    private String segredo;
+    @Value("${jwt.milissegundos}")
+    private Long milissegundo;
+
+    public String gerarToken(String username, String id) {
+        Date vencimento = new Date(System.currentTimeMillis() + milissegundo);
+
+        String token = Jwts.builder().setSubject(username)
+                .claim("idUsuario", id).setExpiration(vencimento)
+                .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
+
+        return token;
+    }
 }
