9c80ec1386ff0c6c0947f676a93052223dde3bef
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-21 15:41:18 -0300

    Criar metodo de doFilterInternal

--- src/main/java/br/com/zup/CouchZupper/security/JWT/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/CouchZupper/security/JWT/FiltroDeAutorizacaoJWT.java
@@ -1,11 +1,19 @@
 package br.com.zup.CouchZupper.security.JWT;
 
 import br.com.zup.CouchZupper.exception.TokenInvalidoException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
 
@@ -18,8 +26,8 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         this.userDetailsService = userDetailsService;
     }
 
-    public UsernamePasswordAuthenticationToken pegarAutenticacao (String token) {
-        if(jwtComponent.tokenValido(token)){
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token) {
+        if (jwtComponent.tokenValido(token)) {
             throw new TokenInvalidoException();
         }
 
@@ -29,4 +37,21 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
     }
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Token ")) {
+            try {
+                UsernamePasswordAuthenticationToken auth = pegarAutenticacao(token.substring(6));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (TokenInvalidoException exception) {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
 }
