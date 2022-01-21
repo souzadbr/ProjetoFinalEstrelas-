7fdb90136f823ab223d60a54994fc6bc0506d391
vanessalb08 <vanessalimab08@gmail.com>
2022-01-21 16:27:07 -0300

    Criar verificação de email e telefone existentes

--- src/main/java/br/com/zup/CouchZupper/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/UsuarioRepository.java
@@ -3,4 +3,6 @@ package br.com.zup.CouchZupper.usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+    boolean existsByEmail (String email);
+    boolean existsByTelefone(String telefone);
 }
--- src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
@@ -1,7 +1,11 @@
 package br.com.zup.CouchZupper.usuario;
 
+import br.com.zup.CouchZupper.exception.EmailJaCadastradoException;
+import br.com.zup.CouchZupper.exception.TelefoneJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -10,7 +14,21 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public Usuario salvarUsuario(Usuario novoUsuario){
+        if(verificarEmailExistente(novoUsuario.getEmail())){
+            throw new EmailJaCadastradoException();
+        }
+        if(  verificarTelefoneExistente(novoUsuario.getTelefone())){
+            throw new TelefoneJaCadastradoException();
+        }
         return usuarioRepository.save(novoUsuario);
 
+    }
+
+    public boolean verificarEmailExistente (String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public boolean verificarTelefoneExistente (String telefone){
+        return usuarioRepository.existsByTelefone(telefone);
     }
 }
