becf5b606feca135d2c65786ee260d295b2422f4
Debora Souza <deboral.p@hotmail.com>
2022-01-21 11:22:48 -0300

    Criar interface Repository para Usuario

--- src/main/java/usuario/UsuarioRepository.java
+++ src/main/java/usuario/UsuarioRepository.java
@@ -1,2 +1,6 @@
-package usuario;public class UsuarioRepository {
+package usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
 }
