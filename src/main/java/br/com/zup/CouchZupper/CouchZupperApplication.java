6c07971fc34d1cb03103b46872f95c316e5eb4d3
Debora Souza <deboral.p@hotmail.com>
2022-01-21 10:27:28 -0300

    Criar Entidade Usuario

---
+++ src/main/java/br/com/zup/CouchZupper/CouchZupperApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.CouchZupper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchZupperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchZupperApplication.class, args);
+	}
+
+}
---
+++ src/main/java/usuario/Usuario.java
@@ -0,0 +1,86 @@
+package usuario;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private int idade;
+    @Column(nullable = false, unique = true)
+    private String telefone;
+    @Column(nullable = false)
+    private Genero genero;
+    @Column(nullable = false)
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
