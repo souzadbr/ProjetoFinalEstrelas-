8ea99986598c81f136b03b951f260370ad50ff4c
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-21 16:26:17 -0300

    Criar criptografia de senha login

--- src/main/java/br/com/zup/CouchZupper/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/CouchZupper/security/ConfiguracaoDeSeguranca.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -54,6 +55,11 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
         configuration.addAllowedHeader("*");
         corsConfigurationSource.registerCorsConfiguration("/**", configuration);
         return corsConfigurationSource;
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
