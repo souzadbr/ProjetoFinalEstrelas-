f8cc61f58a6d1f701590543155c9e02958227973
Debora Souza <deboral.p@hotmail.com>
2022-01-24 11:35:15 -0300

    Configurar tratamento de erros de validacao

--- src/main/java/br/com/zup/CouchZupper/configs/ControllerAdvisor.java
+++ src/main/java/br/com/zup/CouchZupper/configs/ControllerAdvisor.java
@@ -1,7 +1,28 @@
 package br.com.zup.CouchZupper.configs;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvisor {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> manipularErrosValidacao(MethodArgumentNotValidException exception){
+        List<ErroDeValidacao>erros=new ArrayList<>();
+        for (FieldError fielError: exception.getFieldErrors()) {
+            ErroDeValidacao erroDeValidacao = new ErroDeValidacao(fielError.getField(), fielError.getDefaultMessage());
+            erros.add(erroDeValidacao);
+        }
+        return erros;
+    }
+
 }
