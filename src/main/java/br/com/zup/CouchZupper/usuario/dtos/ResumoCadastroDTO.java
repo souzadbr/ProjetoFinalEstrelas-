d26cc5004028fdc22e17cba53edc83bf5988d7af
Debora Souza <deboral.p@hotmail.com>
2022-01-24 11:03:14 -0300

    Adicionar anotacoes de validacao na ResumoCadastroDTO

--- src/main/java/br/com/zup/CouchZupper/usuario/dtos/ResumoCadastroDTO.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/dtos/ResumoCadastroDTO.java
@@ -6,13 +6,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResumoCadastroDTO {
     private String id;
+    @Size(min=2, max=50, message = "{validacao.nome.size}")
+    @NotNull(message = "{validacao.nome.not-null}")
+    @NotBlank(message = "{validacao.nome.not-blank}")
     private String nome;
+    @DecimalMin(value = "18",message = "{validacao.idade.decimal-min}")
     private int idade;
+    @Valid
     private Estado estado;
 }
