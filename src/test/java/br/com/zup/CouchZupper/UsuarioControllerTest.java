8b512a3769199030b5f3f0bc9217b4c20a70da10
Debora Souza <deboral.p@hotmail.com>
2022-01-24 17:20:11 -0300

    Testar rota para cadastro usuario validando Enum de Estado em branco

--- src/test/java/br/com/zup/CouchZupper/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/CouchZupper/UsuarioControllerTest.java
@@ -245,4 +245,16 @@ public class UsuarioControllerTest {
                 .andExpect(MockMvcResultMatchers.status().is(422));
     }
 
+    @Test
+    @WithMockUser ("user@user.com")
+    public void testarRotaParaCadastrarEstadoEmBracno()throws Exception{
+        Mockito.when(usuarioService.salvarUsuario(Mockito.any(Usuario.class))).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(usuario);
+        json = json.replace("ACRE"," ");
+
+        ResultActions resultado = mockMvc.perform(MockMvcRequestBuilders.post("/usuario")
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(422));
+    }
+
 }
