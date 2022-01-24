cbb31dd0f6cc4166be9f40514e2ae037c1a9c334
vanessalb08 <vanessalimab08@gmail.com>
2022-01-24 10:08:58 -0300

    Adicionar dependencias

--- src/main/java/br/com/zup/CouchZupper/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/CouchZupper/security/ConfiguracaoDeSeguranca.java
@@ -43,7 +43,7 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    private CorsConfigurationSource configurarCORS() {
+    CorsConfigurationSource configurarCORS() {
         UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedMethod("*");
--- src/main/java/br/com/zup/CouchZupper/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/UsuarioRepository.java
@@ -4,8 +4,10 @@ import br.com.zup.CouchZupper.enums.Estado;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+    Optional<Usuario> findByEmail(String email);
     boolean existsByEmail (String email);
     boolean existsByTelefone(String telefone);
     List<Usuario> findAllByEstado (Estado estado);
