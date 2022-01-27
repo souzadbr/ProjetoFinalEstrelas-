1a70aa6ceeab3af5559e5408ecd87996384c4dab
vanessalb08 <vanessalimab08@gmail.com>
2022-01-26 23:29:59 -0300

    Criar filtro de preferencias

--- src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaService.java
+++ src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaService.java
@@ -1,9 +1,12 @@
 package br.com.zup.CouchZupper.preferencia;
 
+import br.com.zup.CouchZupper.enums.TipoDePet;
 import br.com.zup.CouchZupper.exception.PreferenciaNaoLocalizadaException;
+import br.com.zup.CouchZupper.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +20,12 @@ public class PreferenciaService {
             throw new PreferenciaNaoLocalizadaException();
         }
         return preferenciaOptional.get();
+
+    }
+
+    public List<Preferencia> listarPreferencias(){
+        Iterable<Preferencia> listaPreferencias = preferenciaRepository.findAll();
+        return (List<Preferencia>) listaPreferencias;
     }
 
     public Preferencia atualizarPreferencias (int id, Preferencia preferencia){
@@ -30,5 +39,21 @@ public class PreferenciaService {
 
         preferenciaRepository.save(preferenciaAtualizar);
         return preferenciaAtualizar;
+    }
+
+    public List<Preferencia> buscarPreferencias(Boolean temPet, TipoDePet tipoDePet, Boolean fumante, Boolean disponivel, Usuario usuario){
+        if (temPet != null){
+            if (usuario.getPreferencia().isTemPet() == temPet){
+                return preferenciaRepository.findAllByTemPet(temPet);
+            }
+        } else if (tipoDePet != null){
+            return preferenciaRepository.findAllByTipoDePet(tipoDePet);
+        }else if (fumante != null){
+            return preferenciaRepository.findAllByFumante(fumante);
+        }else if (disponivel != null){
+            return preferenciaRepository.findAllByDisponivelParaReceberUmZupper(disponivel);
+        }
+
+        return listarPreferencias();
     }
 }
