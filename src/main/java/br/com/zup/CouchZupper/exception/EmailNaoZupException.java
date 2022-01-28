bec4ae46fed3784f0fd8d8bca8cdb98fbcb38eae
Debora Souza <deboral.p@hotmail.com>
2022-01-28 11:49:04 -0300

    Criar exception personalizada EmailNaoZupException.java

--- src/main/java/br/com/zup/CouchZupper/exception/EmailNaoZupException.java
+++ src/main/java/br/com/zup/CouchZupper/exception/EmailNaoZupException.java
@@ -1,2 +1,4 @@
-package br.com.zup.CouchZupper.exception;public class EmailNaoZupException {
+package br.com.zup.CouchZupper.exception;
+
+public class EmailNaoZupException extends RuntimeException{
 }
