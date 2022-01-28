9cd0bd6d1d853d6ae009b06f10f8d0184dcf538d
Debora Souza <deboral.p@hotmail.com>
2022-01-28 16:35:53 -0300

    Importar exception personalizada EmailNaoZupException

--- src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/UsuarioService.java
@@ -3,6 +3,7 @@ package br.com.zup.CouchZupper.usuario;
 import br.com.zup.CouchZupper.enums.Estado;
 import br.com.zup.CouchZupper.enums.Genero;
 import br.com.zup.CouchZupper.exception.EmailJaCadastradoException;
+import br.com.zup.CouchZupper.exception.EmailNaoZupException;
 import br.com.zup.CouchZupper.exception.TelefoneJaCadastradoException;
 import br.com.zup.CouchZupper.exception.UsuarioNaoLocalizadoException;
 import br.com.zup.CouchZupper.usuario.dtos.UsuarioRequisicaoDTO;
