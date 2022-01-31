7029b9de7838615c1ca996e055a065edc76f84f3
vanessalb08 <vanessalimab08@gmail.com>
2022-01-31 16:18:56 -0300

    Habilitar Feign na aplicacao

--- src/main/java/br/com/zup/CouchZupper/CouchZupperApplication.java
+++ src/main/java/br/com/zup/CouchZupper/CouchZupperApplication.java
@@ -2,8 +2,10 @@ package br.com.zup.CouchZupper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CouchZupperApplication {
 
 	public static void main(String[] args) {
