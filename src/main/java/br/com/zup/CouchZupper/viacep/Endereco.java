a6ea7eca13bc807220c0585b9a0dbcf0b268644c
vanessalb08 <vanessalimab08@gmail.com>
2022-01-31 16:22:46 -0300

    Criar classe Endereco

---
+++ src/main/java/br/com/zup/CouchZupper/viacep/Endereco.java
@@ -0,0 +1,10 @@
+package br.com.zup.CouchZupper.viacep;
+
+import lombok.Getter;
+
+@Getter
+public class Endereco {
+    private String cep;
+    private String localidade;
+    private String uf;
+}
