88d45d4cfedcc31fca64d0d217f067c9e0f4fb33
Arê Nascimento <aretuza.gab@gmail.com>
2022-01-31 14:27:56 -0300

    Criar teste de buscar preferencia por id caminho feliz

--- src/test/java/br/com/zup/CouchZupper/preferencia/PreferenciaServiceTest.java
+++ src/test/java/br/com/zup/CouchZupper/preferencia/PreferenciaServiceTest.java
@@ -1,10 +1,20 @@
 package br.com.zup.CouchZupper.preferencia;
 
 import br.com.zup.CouchZupper.enums.TipoDePet;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 public class PreferenciaServiceTest {
@@ -29,6 +39,11 @@ public class PreferenciaServiceTest {
 
     }
 
+    @Test
+    public void testarBuscarPreferenciaPorIdCaminhoFeliz() {
+        Mockito.when(preferenciaRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(preferencia));
+        Preferencia buscarPreferenciaPorId = preferenciaService.buscarPreferenciaPorId(Mockito.anyInt());
+        Assertions.assertEquals(preferencia.getId(), buscarPreferenciaPorId.getId());
 
-
+    }
 }
