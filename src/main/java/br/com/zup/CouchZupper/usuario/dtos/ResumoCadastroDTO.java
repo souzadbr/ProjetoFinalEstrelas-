323f9ca59a8e301d047bfe3ec94ac874c9e365e8
Debora Souza <deboral.p@hotmail.com>
2022-02-01 17:51:20 -0300

    Configurar padrao de Json para LocalDate

--- src/main/java/br/com/zup/CouchZupper/usuario/dtos/ResumoCadastroDTO.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/dtos/ResumoCadastroDTO.java
@@ -2,6 +2,10 @@ package br.com.zup.CouchZupper.usuario.dtos;
 
 import br.com.zup.CouchZupper.enums.Estado;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +34,9 @@ public class ResumoCadastroDTO {
     private String localidade;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
     @JsonFormat( pattern = "yyyy/MM/dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(  shape = JsonFormat.Shape.STRING,pattern = "yyyy/MM/dd")
     private LocalDate dataNascimento;
     @Valid
     @NotNull(message = "{validacao.estado.not-null}")
