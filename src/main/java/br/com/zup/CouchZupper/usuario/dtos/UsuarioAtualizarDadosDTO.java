872a29bfd8d7486c003b584cbfa37b18adbec02d
Debora Souza <deboral.p@hotmail.com>
2022-02-01 17:52:28 -0300

    Configurar padrao de Json para LocalDate

--- src/main/java/br/com/zup/CouchZupper/usuario/dtos/UsuarioAtualizarDadosDTO.java
+++ src/main/java/br/com/zup/CouchZupper/usuario/dtos/UsuarioAtualizarDadosDTO.java
@@ -3,6 +3,10 @@ package br.com.zup.CouchZupper.usuario.dtos;
 import br.com.zup.CouchZupper.enums.Estado;
 import br.com.zup.CouchZupper.enums.Genero;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +18,9 @@ import java.time.LocalDate;
 @NoArgsConstructor @AllArgsConstructor
 public class UsuarioAtualizarDadosDTO {
     private String nome;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(  shape = JsonFormat.Shape.STRING,pattern = "yyyy/MM/dd")
     private LocalDate dataNascimento;
     private int idade;
     private String telefone;
