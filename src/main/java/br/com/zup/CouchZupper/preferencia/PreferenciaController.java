9df084ae460d98318a3fb3d514d5a8903537647e
Arê Nascimento <aretuza.gab@gmail.com>
2022-02-01 15:00:46 -0300

    Retirar metodo get da controller preferencia

--- src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaController.java
+++ src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaController.java
@@ -1,14 +1,9 @@
 package br.com.zup.CouchZupper.preferencia;
 
-import br.com.zup.CouchZupper.enums.TipoDePet;
 import br.com.zup.CouchZupper.preferencia.dtos.PreferenciaEntradaDTO;
-import br.com.zup.CouchZupper.usuario.Usuario;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/preferencias")
@@ -19,24 +14,9 @@ public class PreferenciaController {
     @Autowired
     private ModelMapper modelMapper;
 
-    @GetMapping
-    public List<PreferenciaEntradaDTO> buscarPreferencias (@RequestParam(required = false) Boolean temPet,
-                                                           @RequestParam(required = false) TipoDePet tipoDePet,
-                                                           @RequestParam(required = false) Boolean fumante,
-                                                           @RequestParam(required = false) Boolean disponivel,
-                                                           @RequestParam(required = false) Usuario usuario) {
-        List<PreferenciaEntradaDTO> listaResumo = new ArrayList<>();
-        for(Preferencia preferencia : preferenciaService.buscarPreferencias(temPet,tipoDePet, fumante,
-                disponivel, usuario)) {
-            PreferenciaEntradaDTO resumo = modelMapper.map(preferencia, PreferenciaEntradaDTO.class);
-            listaResumo.add(resumo);
-        }
-        return listaResumo;
-    }
-
-    @PutMapping ("/{id}")
+    @PutMapping("/{id}")
     public void atualizarPreferencia(@PathVariable int id,
-                                     @RequestBody PreferenciaEntradaDTO preferenciaEntradaDTO){
+                                     @RequestBody PreferenciaEntradaDTO preferenciaEntradaDTO) {
         Preferencia preferencia = modelMapper.map(preferenciaEntradaDTO, Preferencia.class);
         preferenciaService.atualizarPreferencias(id, preferencia);
     }
