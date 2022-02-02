5daf88f24db2afc9263b0ecd9dac018a8d043d84
Arê Nascimento <aretuza.gab@gmail.com>
2022-02-02 10:39:40 -0300

    Deletar metodos/testes não utilizados

--- src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaRepository.java
+++ src/main/java/br/com/zup/CouchZupper/preferencia/PreferenciaRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PreferenciaRepository extends CrudRepository <Preferencia, Integer> {
-    List<Preferencia> findAllByTemPet (Boolean temPet);
-    List<Preferencia> findAllByFumante (Boolean fumante);
-    List<Preferencia> findAllByDisponivelParaReceberUmZupper (Boolean disponivel);
-    List<Preferencia> findAllByTipoDePet (TipoDePet tipoDePet);
+public interface PreferenciaRepository extends CrudRepository<Preferencia, Integer> {
+
 }
